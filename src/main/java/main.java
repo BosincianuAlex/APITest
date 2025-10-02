67c2d99ceed72eae1ccc4cb0d420efb71cc73170
BosincianuAlex <bos.roberto@ymail.com>
2025-10-02 22:13:24 +0300

    change

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,11 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import pojo.Pokemon;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -13,11 +17,17 @@ public static void main() throws IOException {
 
     Pokemon pokemon = given().when().get().then().extract().body().as(Pokemon.class);
 
-    //System.out.print( STR."\{pokemon.getName()}'s ablities: \n" );
-    //pokemon.getMoves().forEach(i -> System.out.println(i.getMove().getName()));
+    System.out.print( STR."\{pokemon.getName()}'s ablities: \n" );
+    pokemon.getMoves().forEach(i -> System.out.println(i.getMove().getName()));
 
     System.out.print( STR."\{pokemon.getName()}'s base stats: \n" );
-    pokemon.getStats().forEach(i-> System.out.println( i.getStat().getName()+ " "+i.getBaseStat() ));
+    pokemon.getStats().forEach(i-> System.out.println(STR."\{i.getStat().getName()} \{i.getBaseStat()}"));
 
+    RestAssured.baseURI= "https://gen-endpoint.com/api/echo";
 
+    String response = given().body(pokemon).when().post().then().extract().response().asString();
+
+    List<Map<String, Object>> object = new JsonPath(response).getList("receivedBody.stats");
+    System.out.println(object.get(0));
+    object.forEach(i-> System.out.println(i.get("base_stat")));
 }
